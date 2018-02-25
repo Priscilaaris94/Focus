0b98f673948f2ea4a46eb577084e43eb1bf4d2d5
priscila <deavila.priscila@outlook.com>
2018-02-25 12:40:01 -0800

    point

---
+++ app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import fusioncharts from 'fusioncharts';
+// Load the charts module
+import charts from 'fusioncharts/fusioncharts.charts';
+import react-fusioncharts from 'react-fusioncharts';
+
+// Pass fusioncharts as a dependency of charts
+charts(FusionCharts)
